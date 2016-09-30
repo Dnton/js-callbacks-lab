d879bb3e18d6cbf7d4b3e0767c61e15eda5d7ee3
MacBook Pro <MacBookPro@MacBook-Pros-MacBook-Pro.local>
2016-09-30 08:50:38 +0800

    submitted version

--- starter-code/timers.js
+++ starter-code/timers.js
@@ -1,1 +1,34 @@
 // your code here:
+
+var seconds = 0
+
+var timerId = ''
+
+var time = document.querySelector('#timer')
+
+var start
+
+// function updateTime() {
+//   var time = document.querySelector('#timer')
+//   time.textContent = 'Time Elapsed: ' + seconds;
+//   seconds++;
+// }
+
+document.querySelector('#reset').addEventListener('click', function(){
+  clearInterval(start)
+  seconds = 0
+  time.textContent = 'Stop Watch'
+});
+
+document.querySelector('#start').addEventListener('click', function() {
+  start = window.setInterval(updateTime, 1000)
+});
+
+function updateTime() {
+  time.textContent = 'Time Elapsed: ' + seconds;
+  seconds++;
+}
+
+document.querySelector('#pause').addEventListener('click', function() {
+  clearInterval(start)
+});
